6e6213f79af1a3c95f211c9b32938fabc14a9952
hutoTUM <hutoTUM@users.noreply.github.com>
2016-08-18 12:19:31 +0200

    Switch from function pass to module pass

--- lib/delete.cpp
+++ lib/delete.cpp
@@ -1,22 +1,26 @@
 #include "llvm/IR/Function.h"
+#include "llvm/IR/Module.h"
 #include "llvm/Pass.h"
 #include "llvm/Support/CommandLine.h"
 #include "llvm/Support/raw_ostream.h"
 
 namespace {
 
-static llvm::cl::opt<std::string> EncapsulatedFunction(
-    "deletefunction",
-    llvm::cl::desc("Name of the function to be deleted"),
+static llvm::cl::opt<std::string> DeletedFunction(
+    "deletefunction", llvm::cl::desc("Name of the function to be deleted"),
     llvm::cl::Required);
 
-struct DeleteDefinition : public llvm::FunctionPass {
+struct DeleteDefinition : public llvm::ModulePass {
   static char ID;
-  DeleteDefinition() : FunctionPass(ID) {}
+  DeleteDefinition() : llvm::ModulePass(ID) {
+    /* empty constructor, just call the parent's one */
+  }
 
-  bool runOnFunction(llvm::Function &F) override {
-    if (F.getName() == EncapsulatedFunction) {
-      F.deleteBody();
+  bool runOnModule(llvm::Module& M) override {
+    llvm::Function* func = M.getFunction(DeletedFunction);
+
+    if (func) {
+      func->deleteBody();
       // This function was modified
       return true;
     }
@@ -24,8 +28,13 @@ struct DeleteDefinition : public llvm::FunctionPass {
     return false;
   }
 };
-} // namespace
+}  // namespace
 
+
+// Finally register the new pass
 char DeleteDefinition::ID = 0;
-static llvm::RegisterPass<DeleteDefinition>
-    X("deletedefinition", "Delete the definition of a function", false, false);
+static llvm::RegisterPass<DeleteDefinition> X(
+    "deletedefinition", "Delete the definition of a function",
+    false,  // walks CFG without modifying it?
+    false   // is only analysis?
+    );
